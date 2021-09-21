623daf55b13f9d92efc04ef0f9814bea038433c8
vamsee69 <34119344+vamsee69@users.noreply.github.com>
2021-09-21 20:22:33 +0530

    Day1 Assignment Submitted

---
+++ Day1 Assignment.py
@@ -0,0 +1,6 @@
+# Assignment : Take two inputs and store in variable x & y,find x to the power y
+
+x = int(input('Enter the value of x: '))
+y = int(input('Enter the value of y: '))
+
+print (x ** y)   # x to the power of y 
